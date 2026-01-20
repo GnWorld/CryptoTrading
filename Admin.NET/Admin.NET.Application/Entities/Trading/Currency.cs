8f5d18e4eb60d256bdb9114a4014625f836b6375
王广良 <wangglnet@163.com>
2026-01-20 18:02:48 +0800

    币种表 ，交易品种表设计

---
+++ Admin.NET/Admin.NET.Application/Entities/Trading/Currency.cs
@@ -0,0 +1,56 @@
+﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//
+// 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
+//
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+
+using Admin.NET.Application.Enums;
+using SqlSugar;
+
+namespace Admin.NET.Application.Entity.Trading;
+
+[SugarTable(null,"币种表")]
+public class Currency : EntityBaseTenantOrgDel
+{
+    /// <summary>
+    /// 编码
+    /// </summary>
+    [SugarColumn(ColumnDescription = "币种编码")]
+    public string Code { get; set; }
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    [SugarColumn(ColumnDescription = "币种名称")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 图标
+    /// </summary>
+    [SugarColumn(ColumnDescription = "币种图标")]
+    public string Icon { get; set; }
+
+    /// <summary>
+    /// 币种类型  0-法币 1-虚拟货币
+    /// </summary>
+    [SugarColumn(ColumnDescription = "币种类型 0-法币 1-虚拟货币")]
+    public CurType CurType { get; set; }
+
+    /// <summary>
+    /// 锚定币种代码
+    /// </summary>
+    [SugarColumn(ColumnDescription = "锚定币种代码 默认锚定USD")]
+    public string AnchorCurCode { get; set; }
+
+    /// <summary>
+    /// 锚定汇率
+    /// </summary>
+    [SugarColumn(ColumnDescription = "锚定币种汇率 统一使用 USD/XXX ")]
+    public decimal AnchorRate { get; set; }
+
+    /// <summary>
+    /// 是否刷新锚定汇率  0-否 1-是
+    /// </summary>
+    [SugarColumn(ColumnDescription = "是否刷新汇率")]
+    public bool IsFlushAnchorRate { get; set; }
+}
---
+++ Admin.NET/Admin.NET.Application/Entities/Trading/TradeInstrument.cs
@@ -0,0 +1,115 @@
+﻿using SqlSugar;
+
+namespace Admin.NET.Application.Entities.Trading;
+
+[SugarTable(null, "交易品种")]
+public class TradeInstrument : EntityBaseTenantOrgDel
+{
+    /// <summary>
+    /// 交易对标识（如：BTCUSDT）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "交易对标识")]
+    public string Symbol { get; set; }
+
+    /// <summary>
+    /// 基础币种编码
+    /// </summary>
+    [SugarColumn(ColumnDescription = "基础币种编码")]
+    public string BaseCurCode { get; set; }
+
+    /// <summary>
+    /// 计价币种编码
+    /// </summary>
+    [SugarColumn(ColumnDescription = "计价币种编码")]
+    public string QuoteCurCode { get; set; }
+
+    /// <summary>
+    /// 展示名称（如：BTC/USDT 永续）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "展示名称")]
+    public string DisplayName { get; set; }
+
+    /// <summary>
+    /// 保证金币种（结算币种）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "保证金币种编码")]
+    public string MarginCurCode { get; set; }
+
+    /// <summary>
+    /// 是否永续合约 0-交割合约 1-永续合约
+    /// </summary>
+    [SugarColumn(ColumnDescription = "是否永续合约")]
+    public bool IsPerpetual { get; set; }
+
+    /// <summary>
+    /// 最大杠杆倍数
+    /// </summary>
+    [SugarColumn(ColumnDescription = "最大杠杆倍数")]
+    public int MaxLeverage { get; set; }
+
+    /// <summary>
+    /// 价格精度（小数位）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "价格精度")]
+    public int PricePrecision { get; set; }
+
+    /// <summary>
+    /// 数量精度（小数位）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "数量精度")]
+    public int QuantityPrecision { get; set; }
+
+    /// <summary>
+    /// 最小下单数量
+    /// </summary>
+    [SugarColumn(ColumnDescription = "最小下单数量")]
+    public decimal MinOrderQty { get; set; }
+
+    /// <summary>
+    /// 最大下单数量
+    /// </summary>
+    [SugarColumn(ColumnDescription = "最大下单数量")]
+    public decimal MaxOrderQty { get; set; }
+
+    /// <summary>
+    /// 最小名义金额（计价币种）
+    /// </summary>
+    [SugarColumn(ColumnDescription = "最小名义金额")]
+    public decimal MinNotional { get; set; }
+
+    /// <summary>
+    /// 最小价格步长
+    /// </summary>
+    [SugarColumn(ColumnDescription = "价格步长")]
+    public decimal TickSize { get; set; }
+
+    /// <summary>
+    /// 最小数量步长
+    /// </summary>
+    [SugarColumn(ColumnDescription = "数量步长")]
+    public decimal StepSize { get; set; }
+
+    /// <summary>
+    /// Maker 手续费率
+    /// </summary>
+    [SugarColumn(ColumnDescription = "Maker 手续费率")]
+    public decimal MakerFeeRate { get; set; }
+
+    /// <summary>
+    /// Taker 手续费率
+    /// </summary>
+    [SugarColumn(ColumnDescription = "Taker 手续费率")]
+    public decimal TakerFeeRate { get; set; }
+
+    /// <summary>
+    /// 是否启用
+    /// </summary>
+    [SugarColumn(ColumnDescription = "是否启用")]
+    public bool Enabled { get; set; }
+
+    /// <summary>
+    /// 排序号
+    /// </summary>
+    [SugarColumn(ColumnDescription = "排序号")]
+    public int Sort { get; set; }
+}
